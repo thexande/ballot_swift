6268a4303ed6b41d3afd9f29af1d71ebde52979e
Alexander Murphy <amurphy345@gmail.com>
2016-09-15 12:05:51 -0600

    working on watch speech to text

---
+++ ballot-watch Extension/InterfaceController.swift
@@ -0,0 +1,43 @@
+//
+//  InterfaceController.swift
+//  ballot-watch Extension
+//
+//  Created by Alexander Murphy on 9/14/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    let textChoices: [String] = ["Yes","No","Maybe","I'll think about it","Just Order Pizza", "Out on a run, later dude", "Seriously, just order Pizza"]
+    // my test button
+    @IBOutlet var myLabel: WKInterfaceLabel!
+    
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+//    my test aciton
+    @IBAction func textInputPressed() {
+        presentTextInputControllerWithSuggestions(textChoices,
+                                                  allowedInputMode: WKTextInputMode.Plain,
+                                                  completion: {(results) -> Void in })
+
+    }
+    
+
+}
